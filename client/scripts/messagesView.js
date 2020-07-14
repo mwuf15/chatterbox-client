b69be4670d10845856186163a0a1cc073008b790
Michael wu <mwuf15@gmail.com>
2020-07-13 17:37:59 -0700

    passing add message to DOM spec

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -6,6 +6,16 @@ var MessagesView = {
   },
 
   render: function() {
-  }
+  },
 
+  renderMessage: function(message) {
+    // input is a message object
+    // adds a child to the #chats div
+
+    // use message view's render method to create a new message div
+    var $messageDiv = MessageView.render(message);
+    // get document element by id: chat
+    // prepend message div to that element
+    MessagesView.$chats.prepend($messageDiv);
+  }
 };
